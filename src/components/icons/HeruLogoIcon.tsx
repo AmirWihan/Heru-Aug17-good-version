344fb557138ec3b33cd6500e3cbc55123268f9a2
Amir Wihan <amirwihan@gmail.com>
2025-07-09 23:16:16 +0000

    change the logo for Visafor in crm page and admin page to

--- src/components/icons/HeruLogoIcon.tsx
+++ src/components/icons/HeruLogoIcon.tsx
@@ -3,26 +3,14 @@ import type { SVGProps } from "react";
 export function HeruLogoIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      viewBox="0 0 100 85"
+      viewBox="0 0 100 100"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path
-        d="M15.22,77.5C32.11,48.2,68.89,48.2,85.78,77.5"
-        stroke="hsl(var(--primary))"
-        strokeWidth="15"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M7.5,39.25C28.5,10,72.5,10,93.5,39.25"
-        stroke="hsl(var(--accent))"
-        strokeWidth="15"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <circle cx="50.5" cy="42.5" r="11" fill="hsl(var(--accent))" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M0 0H100V100H0V0Z" fill="#F31E24"/>
+        <path d="M48 42H71L71 21H48V42Z" fill="white"/>
+        <path d="M28 80V42L48 42L48 60H85V80H28Z" fill="white"/>
     </svg>
   );
 }
